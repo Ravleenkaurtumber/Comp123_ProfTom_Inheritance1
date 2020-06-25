e704db420dc7135ca237dcd3c9ff53d6d612b2f3
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-06-25 01:01:01 -0400

    Add project files.

---
+++ Comp123_ProfTom_Inheritance1/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp123_ProfTom_Inheritance1
+{
+    class Person
+    {
+        //PRIVATE INSTANCE VARIABLES(FIELDS)
+        private string m_name;
+        private int m_age;
+        //PUBLIC PROPERTIES
+        public string Name
+        {
+            get//accessor
+            {
+                return m_name;
+
+            }
+            set//mutattor
+            {
+                m_name = value;
+            } 
+        }
+        public int Age {
+            get
+            {
+
+                return m_age;
+            }
+
+            set
+            {
+                m_age = value;
+
+            }
+        }
+        //CONSTRUCTOR
+        Person(string name,int age)
+        {
+            m_name = name;
+            m_age = age;
+        }
+        //PRIVATE METHODS
+        //PUBLIC METHODS
+         public  void SaysHello()//no return tyep so void
+        {
+            Console.WriteLine( $"{Name} says Hello");//if use property m_name it is powerfull
+        }
+    }
+}
---
+++ Comp123_ProfTom_Inheritance1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Comp123_ProfTom_Inheritance1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
