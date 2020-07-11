ec094b43bc5a96dc3b048ab953305be6f07ef99d
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-07-11 14:46:56 -0400

    added domestic students and international

---
+++ Comp123_ProfTom_Inheritance1/DomesticStudent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp123_ProfTom_Inheritance1
+{
+    class DomesticStudent:Student
+    {
+        //PRIVATE INSTANCE MEMEBRS
+        //PUBLIC PROPERTIES
+        public string DomesticAddress { get; set; }
+        //CONSTRUCTOR(S)
+        public DomesticStudent(string name, int age, string student_id,string domestic_address)
+            : base(name, age, student_id)
+        {
+            DomesticAddress = domestic_address;
+
+        }
+        //PRIVATE METHODS
+        //PUBLIC METHODS
+    }
+}
---
+++ Comp123_ProfTom_Inheritance1/InternationalStudent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp123_ProfTom_Inheritance1
+{
+    class InternationalStudent:Student
+    {
+        //PRIVATE  INSTANCE MEMBERS
+        //PUBLIC PROPERTIES
+        public string InternationalAddress{ get; set; }
+        //CONSTRUCTOR
+        public InternationalStudent(string name,int age,string student_id,string international_address)
+            :base(name,age,student_id)
+        {
+            InternationalAddress = international_address;
+        }
+            //PRIVATE METHODS
+            //PUBLIC METHODS
+    }
+}
